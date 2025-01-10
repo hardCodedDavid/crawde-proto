38ac122544cdf115f21da5ed077e45ce55c3f9d0
Sanni David <sannidavidsmart@gmail.com>
2025-01-10 04:53:43 +0100

    feat: design v1.0

--- database/migrations/2024_12_30_123959_create_news_table.php
+++ database/migrations/2024_12_30_123959_create_news_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->text('title')->unique();
+            $table->string('title', 1000)->unique();
             $table->text('description')->nullable();
             $table->string('symbol');
             $table->string('source');
