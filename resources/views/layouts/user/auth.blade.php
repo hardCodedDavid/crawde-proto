f1bc8e4522cc251acc3240ac0ee2e4f954853bca
Sanni David <sannidavidsmart@gmail.com>
2025-01-10 04:11:29 +0100

    feat: design v1.0

--- resources/views/layouts/user/auth.blade.php
+++ resources/views/layouts/user/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>@yield('title_full', 'Crawde') | @yield('title', 'Development Mode')</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta content="Crawde Crypto Builders" name="description" />
     <meta content="Crawde Team" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
