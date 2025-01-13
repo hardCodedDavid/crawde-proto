fcf32ae39c141e78c37b92b07bd69c5188faf399
Sanni David <sannidavidsmart@gmail.com>
2025-01-13 12:43:21 +0100

    fix: tracker

--- resources/views/auth/maintainance.blade.php
+++ resources/views/auth/maintainance.blade.php
@@ -14,21 +14,18 @@
                         <div class="card">                                
                             <div class="card-body p-5">  
                                 <div class="text-center p-3">
-                                    <a href="index.html" class="logo logo-admin">
+                                    <a href="/" class="logo logo-admin">
                                         <img src="https://framerusercontent.com/images/P9hd3hjs5W20ps21ueeNpR2Xo.svg" height="50" alt="logo" class="auth-logo">
                                     </a>
                                     <h4 class="mt-3 mb-1 fw-semibold text-white fs-18">Crawde</h4>
                                 </div>
                                 <div class="text-center mt-3">
                                     <img src="{{ asset('assets/images/extra/card/maintenance.png') }}" alt="" class="img-fluid mb-3">
-                                    <h6 class="mb-2 fw-medium text-dark fs-24">We are down on maintenance right now</h6>
-                                    <p class="text-muted mb-0 text-wrap fs-15">This website is currently undergoing Scheduled maintenance.</p>
-                                    <!-- <div class="mt-3 text-center">
-                                        <a class="btn btn-primary w-50" href="index.html">Back to Dashboard <i class="fas fa-redo ms-1"></i></a> 
-                                    </div> -->
-                                    <span class="badge rounded text-primary bg-primary-subtle w-100 btn-lg fs-14 fw-bold mt-4 py-2">
-                                        Check back later
-                                    </span>
+                                    <h6 class="mb-2 fw-medium text-dark fs-18">We are down on maintenance right now</h6>
+                                    <p class="text-muted mb-0 text-wrap fs-14">This website is currently undergoing Scheduled maintenance. Contact the developers</p>
+                                    <div class="mt-3 text-center">
+                                        <a class="btn btn-primary w-50" href="/">Back to Dashboard <i class="fas fa-redo ms-1"></i></a> 
+                                    </div>
                                 </div> 
                             </div><!--end card-body-->
                         </div><!--end card-->
